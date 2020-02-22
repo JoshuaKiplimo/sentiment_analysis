ed75cbd36286ee10e33982d852722da4c7dcc3f3
JoshuaKiplimo <joshuakiprono8@gmail.com>
2020-02-21 20:05:32 -0500

    Minor update

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def update_output_div(input_value):
                         id ='graph',
                         figure = {
                         'data' : [
-                         {'x': ['Positive', 'Negative', 'Neutral'], 'y':[positive, negative, neutral], 'type':'bar', 'name': 'twitter','marker':dict(color=“Yellow”)}
+                         {'x': ['Positive', 'Negative', 'Neutral'], 'y':[positive, negative, neutral], 'type':'bar', 'name': 'twitter','marker': {'color': ['#b50000', '#e63535', '#fa8989']}}
 
 
                         ],
