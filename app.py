a384d04c5945bc67e270fa2e487abe00519e4758
JoshuaKiplimo <joshuakiprono8@gmail.com>
2020-03-12 22:54:53 -0400

    update

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ app.layout = html.Div([
                     html.P('To make this possible, most subjective data retrieved from Twitter API is analysed for keywords that express sentiments from texts.'
                     ),
                     html.P('Type in data followed by an asterisk, for example Joshua*  - to prevent throttling of API requests.'),
-                    
+
                     html.H5('Type in name of person or company'),
                     dcc.Input(id='my-id',
 
-                 type='text', 
+                 type='text',
                  placeholder='Enter Name'
                  ),
                     #html.P('JKiprono')
@@ -73,14 +73,14 @@ def update_output_div(input_value):
 
         if input_value[-1] == '*':
             input_value = input_value.split('*')[0]
-            
-            
-        
+
+
+
             api_key = os.environ.get('API_KEY')
-            
+
             api_secret_key = os.environ.get('API_SECRET_KEY')
 
-            access_token =os.environ.get('ACECSS_TOKEN') 
+            access_token =os.environ.get('ACECSS_TOKEN')
 
 
             access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
@@ -100,37 +100,30 @@ def update_output_div(input_value):
             tweets = []
             for num in range(len(r['statuses'])):
                 tweets.append(r['statuses'][num]['text'])
-            
-            #subjectivity ranges from 0 to 1 where 0 is very objective and 1 is very subjective 
-            #polarity
-            #greater than 0 is positive
-            # less than 0 is negative 
-            #and 0 is neutral 
+
+            #subjectivity ranges from 0 to 1 where 0 is very objective and 1 is very subjective
+            #polarity lies in the range of [-1,1], where 1 is Very Positive, -1 is very negtive
+
             positive_count = 1 #for now, I will use 1 as a place holder, Needs major update as it skews returned data
             negative_count = 1
             neutral_count = 1
-            
-            texts = 0 # to find out how many texts were actually used in the survey.
             # tweets = twitterdata.get_twitter_data()
             for t in tweets:
-                texts +=1 
                 analysis = TextBlob(t)
-                if analysis.sentiment.polarity > 0.1:
-
-                    if analysis.sentiment.polarity > 0 and analysis.sentiment.subjectivity < 0.7: #increase subjectivity 
+                    if analysis.sentiment.polarity > 0 and analysis.sentiment.subjectivity < 0.7: #increase subjectivity
                         positive_count += 1
-                elif analysis.sentiment.polarity < 0.1:
-                    if analysis.sentiment.polarity < 0 and analysis.sentiment.subjectivity < 0.7:
-                        negative_count += 1 
-                else:
-                    neutral_count += 1
+                    elif analysis.sentiment.polarity < 0 and analysis.sentiment.subjectivity < 0.7:
+                        negative_count += 1
+                    else:
+                        neutral_count += 1
+
             total = positive_count + negative_count + neutral_count
             positive = (positive_count/total)*100
             print('positive count:', positive_count,"total", total,"positive",positive)
             negative = (negative_count/total)*100
-            
+
             neutral = (neutral_count/total)*100
-            
+
             return dcc.Graph(
                         id ='graph',
                         figure = {
@@ -148,8 +141,8 @@ def update_output_div(input_value):
                         'yaxis':{
                              'title':'Percentage Of Sentiment'
                                 },
-                        
-                        
+
+
 
 
 
